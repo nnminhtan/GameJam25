e97e5ff2e5b6ebb659ab2752a16fcf9aeeb23902
nkhoaa <131610131+nkhoaa@users.noreply.github.com>
2025-01-18 20:25:05 +0700

    Add collectibles items

---
+++ bubble/Collectibles/item_2.gd
@@ -0,0 +1,40 @@
+extends Node2D
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var interaction_area: InteractionArea = $InteractionArea
+@onready var growth_timer: Timer = $growth_timer
+@onready var collision_shape_2d: CollisionShape2D = $CharacterBody2D/CollisionShape2D
+
+
+var state = "harvested" # not harvested and harvested
+var interaction_manager = null
+
+func _ready() -> void:
+	interaction_manager= get_node_or_null("/root/InteractionManager")
+	if not interaction_manager:
+		print("error: interactionmanager not found")
+		return
+	
+	if state == "harvested":
+		growth_timer.start()
+		
+	interaction_area.interact = Callable(self, "_on_interact")
+	growth_timer.connect("timeout", Callable(self, "_on_timer_timeout"))
+
+func _on_interact():
+	animated_sprite_2d.hide()
+	collision_shape_2d.disabled = true
+	interaction_area.set_deferred("disabled", true)
+	state == "harvested"
+	print("+1 item 2")
+	if interaction_manager:
+		interaction_manager.unregister_area(interaction_area)
+		
+	growth_timer.start()
+
+func _on_timer_timeout():
+	animated_sprite_2d.show()
+	collision_shape_2d.disabled = false
+	interaction_area.set_deferred("disabled", false)
+	
+	state = "not harvested"
--- bubble/Interaction/Interaction area/interaction_area.gd
+++ bubble/Interaction/Interaction area/interaction_area.gd
@@ -3,6 +3,9 @@ class_name InteractionArea
 
 @export var action_name : String = "interact"
 
+# variable plan to use for item interact only
+var is_interactive : bool = true 
+
 var interact : Callable = func():
 	pass
 
@@ -12,4 +15,9 @@ func _on_body_entered(body: Node2D) -> void:
 
 
 func _on_body_exited(body: Node2D) -> void:
+	InteractionManager.unregister_area(self)
+	
+
+func disable_interaction():
+	is_interactive = false
 	InteractionManager.unregister_area(self)
